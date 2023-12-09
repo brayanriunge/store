f956d035fd08f5a74d6b798a3a449177b3fdffb0
RiugeBrian <riungebrayandev@gmail.com>
2023-12-09 12:15:25 +0300

    cloudinary

--- pages/api/admin/register.ts
+++ pages/api/admin/register.ts
@@ -1,4 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
