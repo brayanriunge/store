bdf7034dd13fad07eadd0ef0a2515f7bcc5c440b
RiugeBrian <riungebrayandev@gmail.com>
2023-12-13 18:26:18 +0300

    getProduct backend

---
+++ pages/api/client/getProduct.ts
@@ -0,0 +1,24 @@
+import { prisma } from "@/utils/db";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET")
+    return res.status(405).json({ message: "method not allowed" });
+
+  try {
+    const data = await prisma.product.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    console.log(data);
+    if (data)
+      return res.status(201).json({ message: "products fetched successful" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "internal server error" });
+  }
+}
