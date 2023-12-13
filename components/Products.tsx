e7b4b555519e6bdf5f5bb1ccec526bfa2bbc45a0
RiugeBrian <riungebrayandev@gmail.com>
2023-12-13 18:53:40 +0300

    creating products data interface and setting it to setProducts

--- components/Products.tsx
+++ components/Products.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
+export interface ProductsData {
+  name: string;
+  brand: string;
+  quantity: number;
+  description: string;
+  category: string;
+}
+
 export default function Products() {
+  const [products, setProducts] = useState<ProductsData[]>([]);
   const fetchProducts = async () => {
     const res = fetch("http://localhost:3000/api/client/getProducts");
     const data = (await res).json();
     console.log(data);
+    setProducts(await data);
   };
-  return <div>products</div>;
+  return <div></div>;
 }
