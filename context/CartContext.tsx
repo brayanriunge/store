c01dcfee069158f0dfd473c290cae73f534a2c6f
RiugeBrian <riungebrayandev@gmail.com>
2023-12-21 14:23:21 +0300

    adding context Provider

--- context/CartContext.tsx
+++ context/CartContext.tsx
@@ -1,16 +1,26 @@
+import { createContext } from "react";
 import useLocalStorageState from "use-local-storage-state";
+
+type AddProp = {
+  children: React.ReactNode;
+};
 
 type cartItem = {
   id: string;
   quantity: number;
 };
-type CartContext = {
+type CartContextType = {
   cartItem: cartItem[];
 };
+//creating context
+const CartContext = createContext({});
 
-export default function CartProvider() {
+export default function CartProvider({ children }: AddProp) {
   const [selectedProduct, setSelectedProduct] = useLocalStorageState<
     cartItem[]
   >("cart", { defaultValue: [] });
-  return <div></div>;
+  return;
+  <CartContext.Provider value={{ selectedProduct, setSelectedProduct }}>
+    {children}
+  </CartContext.Provider>;
 }
