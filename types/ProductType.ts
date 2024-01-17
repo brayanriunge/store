ebcfd68bbec1b7785f6b6618f72d80c85661bd27
RiugeBrian <riungebrayandev@gmail.com>
2024-01-17 21:41:34 +0300

    adding types

---
+++ types/ProductType.ts
@@ -0,0 +1,10 @@
+export type ProductData = {
+  id: string;
+  name: string;
+  brand: string;
+  quantity: number;
+  description: string;
+  category: string;
+  imgUrl: string;
+  price: number;
+};
