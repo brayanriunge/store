ce9c74cb1c71c0668d60a34c5600255e20932709
RiugeBrian <riungebrayandev@gmail.com>
2024-01-18 22:36:21 +0300

    designing

--- pages/Product/[id].tsx
+++ pages/Product/[id].tsx
@@ -43,16 +43,22 @@ export default function productSpecific() {
             />
           </div>
           <div className="flex flex-col  w-full lg:w-1/2 lg:py-6 lg:pl-10 ">
-            <h1 className="text-3xl text-bold text-blue-400 ">
+            <h1 className="text-3xl text-bold text-blue-400 m-2 ">
               Name: <span className="font-bold text-black"> {items?.name}</span>
             </h1>
 
-            <p className="justify-content text-2xl">{items?.description}</p>
-            <p className="text-bold">Brand:{items?.brand}</p>
-            <p className="text-blue-700">
+            <p className="justify-content text-2xl m-5">
+              Description:{" "}
+              <span className="font-bold text-black">
+                {" "}
+                {items?.description}
+              </span>
+            </p>
+            <p className="text-bold m-2">Brand:{items?.brand}</p>
+            <p className="text-blue-700 m-2">
               Price: <span className="text-xl text-black"> {items?.price}</span>
             </p>
-            <p className=" text-xl text-blue-400">
+            <p className=" m-2 text-xl text-blue-400">
               Quantity: <span className=" text-black"> {items?.quantity}</span>
             </p>
           </div>
