02d04ddfe2d2a73bb7455fe76a87044319385606
RiugeBrian <riungebrayandev@gmail.com>
2024-01-18 22:48:21 +0300

    designing

--- pages/Product/[id].tsx
+++ pages/Product/[id].tsx
@@ -39,27 +39,38 @@ export default function productSpecific() {
               height={200}
               width={240}
               alt="image"
-              className="h-50 w-full rounded object-cover object-center lg:h-auto lg:w-1/2"
+              className="h-50  rounded object-cover object-center lg:h-auto lg:w-1/2"
             />
           </div>
           <div className="flex flex-col  w-full lg:w-1/2 lg:py-6 lg:pl-10 ">
-            <h1 className="text-3xl text-bold text-blue-400 m-2 ">
+            <h1 className="text-2xl text-bold text-blue-700 m-2 ">
               Name: <span className="font-bold text-black"> {items?.name}</span>
             </h1>
 
-            <p className="justify-content text-2xl m-5">
+            <p className="justify-content text-2xl text-blue-700 m-2">
               Description:{" "}
               <span className="font-bold text-black">
                 {" "}
                 {items?.description}
               </span>
             </p>
-            <p className="text-bold m-2">Brand:{items?.brand}</p>
-            <p className="text-blue-700 m-2">
-              Price: <span className="text-xl text-black"> {items?.price}</span>
+            <p className=" text-blue-700 text-bold text-2xl m-2">
+              Brand:{" "}
+              <span className="font-bold text-black"> {items?.brand}</span>{" "}
             </p>
-            <p className=" m-2 text-xl text-blue-400">
-              Quantity: <span className=" text-black"> {items?.quantity}</span>
+            <p className="text-blue-700 text-2xl m-2">
+              Price:{" "}
+              <span className="text-2xl font-bold text-black">
+                {" "}
+                {items?.price}
+              </span>
+            </p>
+            <p className=" m-2 text-2xl text-blue-700">
+              Quantity:{" "}
+              <span className=" text-black text-2xl font-bold">
+                {" "}
+                {items?.quantity}
+              </span>
             </p>
           </div>
         </div>
