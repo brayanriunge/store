e536eed5ebfeb0fa7dd2b94ef18ae735b579b892
RiugeBrian <riungebrayandev@gmail.com>
2024-01-27 20:56:49 +0300

    creating cart page

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -1,10 +1,35 @@
 import { useCart } from "@/context/CartContext";
+import { ProductsData } from "@/types/ProductType";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 type cartItemProp = {
   id: string;
   quantity: string;
 };
+
 export default function cartItem({ id, quantity }: cartItemProp) {
-  const { addToCart, removeFromCart } = useCart();
-  return <div></div>;
+  const { addToCart, removeFromCart, decreaseCartItem } = useCart();
+  const [item, setItem] = useState<ProductsData | null>(null);
+  const fetchProduct = async () => {
+    const res = await fetch(`http://localhost:3000/api/products/${id}`);
+    const productItem = res.json();
+    setItem(await productItem);
+  };
+  useEffect(() => {
+    fetchProduct();
+  }, [id]);
+
+  return (
+    <section className="flex flex-col items-center justify-between gap-2">
+      <div>
+        <Image
+          src={item?.imgUrl as string}
+          alt="product"
+          height={60}
+          width={60}
+        />
+      </div>
+    </section>
+  );
 }
---
+++ pages/cart.tsx
@@ -0,0 +1,5 @@
+import CartItem from "@/components/CartItem";
+
+export default function Cart() {
+  return <CartItem id={""} quantity={""} />;
+}
