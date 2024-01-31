b4245d2116f41085a3cb1253df909fa8b9ff9146
RiugeBrian <riungebrayandev@gmail.com>
2024-02-01 00:40:00 +0300

    styling

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -29,46 +29,48 @@ export default function cartItem({ id, quantity }: cartItemProp) {
   return (
     <section>
       {item && (
-        <section className=" mx-auto p-5 w-5/6 flex flex-row-5 items-center justify-between gap-5 mt-24 bg-white rounded-sm">
-          <div className="rounded-md mt-6">
-            <Image
-              src={item?.imgUrl as string}
-              alt={item?.name as string}
-              height={60}
-              width={60}
-            />
-          </div>
-          <div className="text-bold text-lg ">{item.brand}</div>
-          <div className=" inline-flex border-4 border-gray-700 ">
-            <button
-              className=" px-5 py-2.5 font-medium "
-              onClick={() => addToCart(item.id)}
-            >
-              <HiOutlinePlus />
-            </button>
-            <div className="px-5 py-2.5 font-medium font-gray-900">
-              {quantity}
+        <section className="  flex flex-row-5 items-center justify-between mt-24">
+          <div className="mx-auto p-5 w-5/6 flex flex-row-5 items-center justify-between gap-5 mt-6 shadow-xl bg-white rounded-lg">
+            <div className="rounded-md mt-6">
+              <Image
+                src={item?.imgUrl as string}
+                alt={item?.name as string}
+                height={60}
+                width={60}
+              />
             </div>
+            <div className="text-bold text-lg ">{item.brand}</div>
+            <div className=" inline-flex border-4 border-gray-700 ">
+              <button
+                className=" px-5 py-2.5 font-medium "
+                onClick={() => addToCart(item.id)}
+              >
+                <HiOutlinePlus />
+              </button>
+              <div className="px-5 py-2.5 font-medium font-gray-900">
+                {quantity}
+              </div>
 
-            <button
-              className="px-5 py-2.5 font-medium "
-              onClick={() => decreaseCartItem(item.id)}
-            >
-              <HiOutlineMinus />
-            </button>
-          </div>
-          <div className="flex">
-            <h1 className="text-md px-4">
-              Price: <span> {item.price * quantity}</span>
-            </h1>
-          </div>
-          <div className="text-md">
-            <button
-              onClick={() => removeFromCart(id)}
-              className="text-red-600 hover:underline"
-            >
-              Remove
-            </button>
+              <button
+                className="px-5 py-2.5 font-medium "
+                onClick={() => decreaseCartItem(item.id)}
+              >
+                <HiOutlineMinus />
+              </button>
+            </div>
+            <div className="flex">
+              <h1 className="text-md px-4">
+                Price: <span> {item.price * quantity}</span>
+              </h1>
+            </div>
+            <div className="text-md">
+              <button
+                onClick={() => removeFromCart(id)}
+                className="text-red-600 hover:underline"
+              >
+                Remove
+              </button>
+            </div>
           </div>
         </section>
       )}
